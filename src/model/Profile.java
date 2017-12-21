a899e41c01163bcdd82aee4f53698f608b04d75a
Djim Oomes <dbc.oomes@student.avans.nl>
2017-12-21 14:02:40 +0100

    Updated profile and added a third constructor

--- src/model/Profile.java
+++ src/model/Profile.java
@@ -1,11 +1,13 @@
 package model;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class Profile {
 
     private String profileName;
     private Date dateOfBirth;
+    private int profileID;
+    private int accountID;
 
     public Profile() {
 
@@ -14,6 +16,20 @@ public class Profile {
     public Profile(String profileName, Date dateOfBirth) {
         this.profileName = profileName;
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public Profile(String profileName, Date dateOfBirth, int profileID) {
+        this.profileName = profileName;
+        this.dateOfBirth = dateOfBirth;
+        this.profileID = profileID;
+    }
+
+    public int getProfileID() {
+        return this.profileID;
+    }
+
+    public void setProfileID(int id) {
+        this.profileID = id;
     }
 
     public String getProfileName() {
@@ -30,5 +46,13 @@ public class Profile {
 
     public void setDateOfBirth(Date date) {
         this.dateOfBirth = date;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
     }
 }
