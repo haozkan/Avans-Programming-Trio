9eabff1cdbec1a850c3fd4e78211f8aa6d5d0471
Djim Oomes <dbc.oomes@student.avans.nl>
2017-12-28 15:18:29 +0100

    Created GUI

--- src/view/UserInterface.java
+++ src/view/UserInterface.java
@@ -1,21 +1,17 @@
 package view;
 
-import java.awt.Container;
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import java.awt.*;
+import javax.swing.*;
 
 public class UserInterface implements Runnable {
 
     private JFrame frame;
-
-    public UserInterface() {
-    }
+    private String series [] = {"test1", "test2", "test3"};
 
     @Override
     public void run() {
-        frame = new JFrame("Title");
-        frame.setPreferredSize(new Dimension(800, 600));
+        frame = new JFrame("Netflix statistix");
+        frame.setPreferredSize(new Dimension(500, 400));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -25,10 +21,53 @@ public class UserInterface implements Runnable {
         frame.setVisible(true);
     }
 
-    private void createComponents(Container container) {
+    private void createComponents(Container container){
+        container.setLayout(new BorderLayout());
+        container.add(leftButtons(), BorderLayout.WEST);
+        container.add(toolbar(), BorderLayout.NORTH);
+        container.add(bottomBar(), BorderLayout.SOUTH);
+        container.add(new JTextArea(),BorderLayout.CENTER);
+
+
     }
 
-    public JFrame getFrame() {
+    public JPanel leftButtons(){
+        //JPanel panel = new JPanel(new GridLayout(3, 1));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        //panel.setPreferredSize(new Dimension(100,400));
+        panel.add(new JButton("Test"));
+        panel.add(new JButton("Test"));
+        panel.add(new JButton("Test"));
+        return panel;
+    }
+
+    public JPanel toolbar(){
+        JPanel upper = new JPanel();
+        upper.setLayout(new FlowLayout());
+        upper.add(new JLabel("Select serie"));
+        upper.add(new JComboBox(series));
+
+        JPanel lower = new JPanel();
+        lower.add(new JLabel("Results: "));
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(upper, BorderLayout.NORTH);
+        panel.add(lower, BorderLayout.SOUTH);
+
+        return panel;
+
+    }
+
+    public JPanel bottomBar(){
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Netflix Statistix"));
+        panel.add(new JLabel("Informatica klas1E - Djim Oomes, Marco van Poortvliet"));
+        return panel;
+    }
+
+    public JFrame getFrame(){
         return frame;
     }
 }
