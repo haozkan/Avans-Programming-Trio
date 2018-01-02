b858108b840cf466adfba3df63d7fb05bf42568f
Djim Oomes <dbc.oomes@student.avans.nl>
2018-01-02 16:58:17 +0100

    Adjusted join query

--- src/datalayer/SerieDAO.java
+++ src/datalayer/SerieDAO.java
@@ -93,7 +93,8 @@ public class SerieDAO implements ISerie {
         try {
             conn = MysqlDAO.getInstance().connect();
             PreparedStatement statement = conn.prepareStatement("SELECT * FROM serie\n" +
-                    "INNER JOIN episode ON episode.serieID = serie.serieID WHERE serie.serieID = ?");
+                    "INNER JOIN episode ON episode.serieID = serie.serieID AND \n " +
+                    "video.videoID = episode.videoID WHERE serie.serieID = ?");
             statement.setInt(1, s.getID());
             ResultSet resultSet = statement.executeQuery();
 
